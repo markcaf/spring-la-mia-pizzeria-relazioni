177b9c9eba4a0c9674db60d24c2808d368f66a6e
Marco Cafasso <marcocafassog@gmail.com>
2022-12-14 16:55:57 +0100

    feat: adds the ingrediente create method with create view
    - updates IngredienteController with createIngrediente and storeIngrediente methods
    - adds the creation form with the checkbox to select which pizza to assign the created ingredient to

--- Pizzeria/src/main/java/org/generation/italy/demo/controller/IngredienteController.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/controller/IngredienteController.java
@@ -3,13 +3,17 @@ package org.generation.italy.demo.controller;
 import java.util.List;
 
 import org.generation.italy.demo.pojo.Ingrediente;
+import org.generation.italy.demo.pojo.Pizza;
 import org.generation.italy.demo.serv.IngredienteService;
 import org.generation.italy.demo.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredienti")
@@ -27,6 +31,29 @@ public class IngredienteController {
 		List<Ingrediente> ingredienti = ingredienteService.findAllWPizza();
 		model.addAttribute("ingredienti", ingredienti);
 		return "ingredienti";
+	}
+	
+	@GetMapping("/ingrediente/create")
+	public String createIngrediente(Model model) {
+
+		Ingrediente ingrediente = new Ingrediente();
+		List<Pizza> pizze = pizzaService.findAll();
+		model.addAttribute("ingrediente", ingrediente);
+		model.addAttribute("pizze", pizze);
+		
+		return "ingrediente-create";
+	}
+	
+	@PostMapping("/ingrediente/create")
+	public String storeIngrediente(@Valid Ingrediente ingrediente) {
+		
+		List<Pizza> ingredientePizze = ingrediente.getPizze();
+		for (Pizza pizza : ingredientePizze) {
+			pizza.getIngredienti().add(ingrediente);
+		}
+		ingredienteService.save(ingrediente);
+		
+		return "redirect:/ingredienti";
 	}
 
 }
